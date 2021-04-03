13ba8d3313f0fca273d1c79732dfe3d271342d96
이병윤 <bur5698@naver.com>
2021-04-03 13:38:19 +0900

    backjoon 21965

---
+++ rescue/rescue16.java
@@ -0,0 +1,56 @@
+package rescue;
+
+import java.util.Scanner;
+
+// 백준
+// 21965
+// https://www.acmicpc.net/problem/21965
+
+public class rescue16 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] value = new int[n];
+
+        String answer = "YES";
+        int index = value.length;
+
+        for(int i=0; i<n; i++){
+            value[i] = Integer.parseInt(sc.next());
+            if(i == 0) continue;
+
+            if(value[i-1] > value[i]) index = i;
+        }
+        
+        int nextChange = 0;
+        for(int i=0; i<index; i++){
+            if(i == 0) continue;
+            if(i == 1){
+                nextChange = value[i] - value[i-1];
+            } else{
+                if(nextChange != value[i] - value[i-1]){
+                    answer = "NO";
+                    break;
+                }
+            }
+        }
+
+        for(int i=index-1; i<value.length; i++){
+            if(i == index-1) continue;
+            if(i == index){
+                nextChange = value[i] - value[i-1];
+            } else{
+                if(nextChange != value[i] - value[i-1]){
+                    answer = "NO";
+                    break;
+                }
+            }
+        }
+
+        if(nextChange == 0) answer = "NO";
+
+        System.out.println(answer);
+
+    }
+}
