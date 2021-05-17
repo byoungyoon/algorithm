0ee1b93a51d51553a5aa42399f2ed5131c1ad6ef
byoungyoon <bur5698@naver.com>
2021-05-17 14:54:21 +0900

    배열

---
+++ Programmers4.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
+// 프로그래머스
+// 음양 더하기
+
+public class Programmers4 {
+    public static void main(String[] args){
+        int[] absolutes = {4,7,12};
+        boolean[] signs = {true,false,true};
+
+        AtomicInteger index = new AtomicInteger();
+
+        System.out.println(Arrays.stream(absolutes).map(data->signs[index.getAndIncrement()]? data: -1*data).sum());
+    }
+}
