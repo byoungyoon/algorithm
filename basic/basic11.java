b240304bfc0b228120097acfa61778bdc810c0c8
byoungyoon <bur5698@naver,com>
2021-05-21 16:00:52 +0900

    수박수박수

---
+++ basic/basic11.java
@@ -0,0 +1,17 @@
+package basic;
+
+// 프로그래머스
+// 수박수박수박수박수
+
+public class basic11{
+    public static void main(String[] args){
+        int n = 4;
+        String[] value = {"수", "박"};
+
+        String answer = "";
+        for(int i=0; i<n; i++){
+            answer += value[i%2];
+        }
+
+    }
+}
