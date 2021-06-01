784062d87949a12e84c856cda74207dfb05894e9
이병윤 <byoungyoon@ejbt.co.kr>
2021-06-01 10:21:52 +0900

    부가항목 추가

---
+++ conversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class conversion {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String value = sc.nextLine();
+        
+        value = value.replaceAll("[\\[]", "{").replaceAll("[\\]]", "}");
+        System.out.println(value);
+    }
+}
