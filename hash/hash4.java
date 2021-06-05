ed25b4466e9322b953d97f70ac6e7e0bba8051dd
byoungyoon <bur5698@naver.com>
2021-06-05 20:12:09 +0900

    영어 끝말잇기

--- hash/hash4.java
+++ hash/hash4.java
@@ -1,8 +1,13 @@
+package hash;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
-public class Test8 {
+// 프로그래머스
+// 영어 끝말잇기
+
+public class hash4 {
     public static void main(String[] args){
         int n = 2;
         String[] words = {"hello", "one", "even", "never", "now", "world", "draw"};
@@ -29,7 +34,7 @@ public class Test8 {
         int second = 0;
 
         if(count != 0 ){
-            frist = count%n==0?3:count%n;
+            frist = count%n==0?n:count%n;
             second = count%n == 0? count/n: count/n+1;
         }
 
