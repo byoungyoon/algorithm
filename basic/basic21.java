00b5d20cfc5e35938785775b0885abf7f32e7119
byoungyoon <bur5698@naver.com>
2021-06-07 10:14:19 +0900

    문자열을 정수로 바꾸기

---
+++ basic/basic21.java
@@ -0,0 +1,12 @@
+package basic;
+
+// 프로그래머스
+// 문자열을 정수로 바꾸기
+
+public class basic21 {
+    public static void main(String[] args){
+        String s = "1234";
+
+        System.out.println(Integer.parseInt(s));
+    }
+}
