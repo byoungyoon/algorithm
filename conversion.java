948f973d67acf479a83a4ecd6fcd231d49709ea3
byoungyoon <bur5698@naver.com>
2021-06-11 13:59:46 +0900

    conversion 오류 수정

--- conversion.java
+++ conversion.java
@@ -5,7 +5,6 @@ public class conversion {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String value = sc.nextLine();
-        `
         value = value.replaceAll("[\\[]", "{").replaceAll("[\\]]", "}");
         System.out.println(value);
     }
