fd5d70a207d67ab370c86fc18053dc9106ba0db7
byoungyoon <bur5698@naver.com>
2022-03-18 13:17:18 +0900

    더 맵게 (우선순위 큐)

---
+++ heap/Heap3.java
@@ -0,0 +1,35 @@
+package heap;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+// https://programmers.co.kr/learn/courses/30/lessons/42626
+
+public class Heap3 {
+    public static void main(String[] args) {
+        int[] scoville = { 1, 2, 3, 9, 10, 12 };
+        int K = 7;
+
+        Heap3 test = new Heap3();
+        System.out.println(test.solution(scoville, K));
+    }
+
+    static Queue<Integer> q = new PriorityQueue<>();
+
+    public int solution(int[] scoville, int K) {
+        for (int i : scoville) {
+            q.add(i);
+        }
+
+        int answer = 0;
+        while (q.peek() < K) {
+            if (q.size() < 2)
+                return -1;
+
+            q.add(q.poll() + (q.poll() * 2));
+            answer++;
+        }
+
+        return answer;
+    }
+}
