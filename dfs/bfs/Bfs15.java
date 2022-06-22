53bcab46a503962cbf773f60ec0b78904dbe0a42
byoungyoon <bur5698@naver.com>
2022-06-22 10:21:56 +0900

    전 커밋 폴더 변경

--- dfs/bfs/Bfs15.java
+++ dfs/bfs/Bfs15.java
@@ -1,3 +1,5 @@
+package dfs.bfs;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,7 +9,7 @@ import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/5014
 
-public class Test {
+public class Bfs15 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -18,7 +20,7 @@ public class Test {
         int U = Integer.parseInt(st.nextToken());
         int D = Integer.parseInt(st.nextToken());
 
-        Test test = new Test();
+        Bfs15 test = new Bfs15();
 
         int answer = test.solution(F, S, G, U, D);
         System.out.println(answer == -1 ? "use the stairs" : answer);
