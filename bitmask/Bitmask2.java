0bd2847be2ff59717fe2a9a90f2c394e7c0498ee
byoungyoon <bur5698@naver.com>
2022-06-28 09:53:55 +0900

    전 커밋 누락

--- bitmask/Bitmask2.java
+++ bitmask/Bitmask2.java
@@ -3,7 +3,6 @@ package bitmask;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Bitmask2 {
